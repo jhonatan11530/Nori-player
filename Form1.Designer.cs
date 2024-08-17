cc7610710df8f65acc0b0dfcc4f25444e43d9701
Jhonatan fernandez muñoz <jhonatan1153@hotmail.com>
2024-08-17 17:42:48 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace Nori_player
+{
+    partial class Nori
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Nori));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkOnlyMusic = new System.Windows.Forms.CheckBox();
+            this.buttonFilePath = new System.Windows.Forms.Button();
+            this.Time_video = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Guardar = new System.Windows.Forms.Button();
+            this.TextFilePath = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboCalidadVideo = new System.Windows.Forms.ComboBox();
+            this.textTitle = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textUrl = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.textUrl);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 450);
+            this.panel1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkOnlyMusic);
+            this.groupBox1.Controls.Add(this.buttonFilePath);
+            this.groupBox1.Controls.Add(this.Time_video);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.Guardar);
+            this.groupBox1.Controls.Add(this.TextFilePath);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.comboCalidadVideo);
+            this.groupBox1.Controls.Add(this.textTitle);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 126);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(776, 312);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Informacion del video";
+            // 
+            // checkOnlyMusic
+            // 
+            this.checkOnlyMusic.AutoSize = true;
+            this.checkOnlyMusic.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkOnlyMusic.ForeColor = System.Drawing.Color.White;
+            this.checkOnlyMusic.Location = new System.Drawing.Point(172, 232);
+            this.checkOnlyMusic.Name = "checkOnlyMusic";
+            this.checkOnlyMusic.Size = new System.Drawing.Size(228, 24);
+            this.checkOnlyMusic.TabIndex = 13;
+            this.checkOnlyMusic.Text = "SOLO MUSICA 320 kbps";
+            this.checkOnlyMusic.UseVisualStyleBackColor = true;
+            // 
+            // buttonFilePath
+            // 
+            this.buttonFilePath.Location = new System.Drawing.Point(533, 160);
+            this.buttonFilePath.Name = "buttonFilePath";
+            this.buttonFilePath.Size = new System.Drawing.Size(129, 23);
+            this.buttonFilePath.TabIndex = 12;
+            this.buttonFilePath.Text = "RUTA DE CARPETA";
+            this.buttonFilePath.UseVisualStyleBackColor = true;
+            this.buttonFilePath.Click += new System.EventHandler(this.buttonFilePath_Click);
+            // 
+            // Time_video
+            // 
+            this.Time_video.AutoSize = true;
+            this.Time_video.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Time_video.ForeColor = System.Drawing.Color.White;
+            this.Time_video.Location = new System.Drawing.Point(656, 58);
+            this.Time_video.Name = "Time_video";
+            this.Time_video.Size = new System.Drawing.Size(79, 20);
+            this.Time_video.TabIndex = 11;
+            this.Time_video.Text = "00:00:00";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(660, 38);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(75, 20);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "TIEMPO";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(250, 273);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(337, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "CREADO POR JHONATAN FERNANDEZ";
+            // 
+            // Guardar
+            // 
+            this.Guardar.Location = new System.Drawing.Point(440, 222);
+            this.Guardar.Name = "Guardar";
+            this.Guardar.Size = new System.Drawing.Size(115, 45);
+            this.Guardar.TabIndex = 8;
+            this.Guardar.Text = "DESCARAGAR";
+            this.Guardar.UseVisualStyleBackColor = true;
+            this.Guardar.Click += new System.EventHandler(this.Guardar_Click);
+            // 
+            // TextFilePath
+            // 
+            this.TextFilePath.Location = new System.Drawing.Point(195, 163);
+            this.TextFilePath.Name = "TextFilePath";
+            this.TextFilePath.Size = new System.Drawing.Size(332, 20);
+            this.TextFilePath.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(20, 163);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(169, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "RUTA A GUARDAR";
+            // 
+            // comboCalidadVideo
+            // 
+            this.comboCalidadVideo.FormattingEnabled = true;
+            this.comboCalidadVideo.Location = new System.Drawing.Point(227, 107);
+            this.comboCalidadVideo.Name = "comboCalidadVideo";
+            this.comboCalidadVideo.Size = new System.Drawing.Size(186, 21);
+            this.comboCalidadVideo.TabIndex = 4;
+            // 
+            // textTitle
+            // 
+            this.textTitle.Location = new System.Drawing.Point(92, 44);
+            this.textTitle.Name = "textTitle";
+            this.textTitle.Size = new System.Drawing.Size(547, 20);
+            this.textTitle.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(20, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(188, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "CALIDAD DEL VIDEO";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(21, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TITULO";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(331, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 42);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Obtener informacion del video";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textUrl
+            // 
+            this.textUrl.Location = new System.Drawing.Point(36, 12);
+            this.textUrl.Name = "textUrl";
+            this.textUrl.Size = new System.Drawing.Size(715, 20);
+            this.textUrl.TabIndex = 0;
+            this.textUrl.Text = "https://www.youtube.com/watch?v=YIgoFjO9Mg4";
+            // 
+            // Nori
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Nori";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Nory player";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textUrl;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboCalidadVideo;
+        private System.Windows.Forms.TextBox textTitle;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Guardar;
+        private System.Windows.Forms.TextBox TextFilePath;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label Time_video;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.CheckBox checkOnlyMusic;
+        private System.Windows.Forms.Button buttonFilePath;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using YoutubeExplode;
+using YoutubeExplode.Videos.Streams;
+using YoutubeExplode.Converter;
+using AngleSharp.Media;
+using System.Threading;
+namespace Nori_player
+{
+    public partial class Nori : Form
+    {
+        private YoutubeClient youtube = new YoutubeClient();
+        public Nori()
+        {
+            InitializeComponent();
+        }
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var url = textUrl.Text;
+
+            var video = await youtube.Videos.GetAsync(url);
+
+            // agregamos el titulo al front
+            textTitle.Text = video.Title;
+            Time_video.Text = video.Duration.ToString();
+
+
+            var streamManifest = await youtube.Videos.Streams.GetManifestAsync(url);
+
+            // Filtra solo las pistas de video
+            var videoStreams = streamManifest.GetVideoOnlyStreams().GetWithHighestVideoQuality();
+
+            // Actualizar el ComboBox de manera segura
+            comboCalidadVideo.Invoke((Action)(() =>
+            {
+                comboCalidadVideo.Items.Clear();
+                comboCalidadVideo.Items.Add($"{videoStreams.VideoQuality.Label}");
+                comboCalidadVideo.SelectedIndex = 0;
+            }));
+        }
+        private void buttonFilePath_Click(object sender, EventArgs e)
+        {
+            using (var folderDialog = new FolderBrowserDialog())
+            {
+                if (folderDialog.ShowDialog() == DialogResult.OK)
+                    TextFilePath.Text = folderDialog.SelectedPath + "\\";
+                else
+                    TextFilePath.Text = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            }
+        }
+        private  void Guardar_Click(object sender, EventArgs e)
+        {
+            Thread hilo = new Thread(download);
+            hilo.IsBackground = true;
+            hilo.Start();
+        }
+        public async void download() {
+            var url = textUrl.Text;
+            var info = await youtube.Videos.GetAsync(url);
+            var streamManifest = await youtube.Videos.Streams.GetManifestAsync(url);
+
+            IStreamInfo streamInfo;
+
+            if (checkOnlyMusic.Checked)
+                streamInfo = streamManifest.GetAudioOnlyStreams().Where(x => x.Bitrate.KiloBitsPerSecond < 150).OrderByDescending(x => x.Bitrate.KiloBitsPerSecond).FirstOrDefault();
+            else
+                streamInfo = streamManifest.GetVideoOnlyStreams().GetWithHighestVideoQuality();
+
+
+            if (streamInfo != null)
+            {
+                await youtube.Videos.Streams.DownloadAsync(streamInfo, $"{TextFilePath.Text}/{info.Title}.{streamInfo.Container}");
+                MessageBox.Show("Se completó la descarga");
+            }
+            else
+            {
+                MessageBox.Show("No se encontró un flujo adecuado para descargar.");
+            }
+        }
+    }
+}
